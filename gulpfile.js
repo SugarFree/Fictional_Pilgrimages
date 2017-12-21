c07f3e8ed7121964c66c3b4d3afc348a2e842c69
SugarFree <34449823+SugarFree@users.noreply.github.com>
2017-12-21 16:25:11 +0100

    Update gulpfile.js
    Fix file .php e .js

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('deploy', function() {
     user: 'fictionalpilgrimages',
     password: 'rangumicpu98'
   });
-  gulp.src(['./*.html', './*.css', './img/*.jpg', './img/*.png'])
+  gulp.src(['./*.html', './*.css', './*.js', './*.php', './img/*.jpg', './img/*.png'])
     .pipe(conn.newer('/'))
     .pipe(conn.dest('/'));
 });
