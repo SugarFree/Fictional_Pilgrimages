55410068f2ffb7cf63e6a1bbf2b0d180786a53b1
Enrico Zen <zenenrico96@gmail.com>
2018-01-02 16:01:20 +0100

    Commit iniziale

---
+++ post.php
@@ -0,0 +1,38 @@
+<?php
+/*Classe Post: Rappresenta un post nel sito.
+* Serve a chi dovrà trasformare il risultato della ricerca (fornito come vettore di oggetti post)
+ *in una pagina html. Questa classe serve solo come ausilio.
+*/
+
+class post
+{
+    var $id;
+    var $titolo_opera;
+    var $descrizione;
+    var $latitudine;
+    var $longitudine;
+    var $username;
+    var $stato;
+    var $indirizzo;
+    var $localita;
+    //var $approvato; Non serve mai mostrarla all'utente
+
+    /**
+     * Costruttore dell'oggetto post.
+     * Tipicamente si presuppone che i dati gli arrivino dal risultato di una query "SELECT", già nel formato corretto,
+     * quindi non ci sono controlli/property/getter/setter ecc.
+     */
+    function __construct($id, $titolo_opera, $descrizione, $latitudine, $longitudine, $username, $stato, $indirizzo, $localita)
+    {
+        $this->id=$id;
+        $this->titolo_opera=$titolo_opera;
+        $this->descrizione=$descrizione;
+        $this->latitudine=$latitudine;
+        $this->longitudine=$longitudine;
+        $this->username=$username;
+        $this->stato=$stato;
+        $this->indirizzo=$indirizzo;
+        $this->localita=$localita;
+    }
+}
+
