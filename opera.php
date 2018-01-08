23f2dba4efa267cee8938ce9405949d496575ea8
Enrico Zen <zenenrico96@gmail.com>
2018-01-08 15:46:34 +0100

    Commit iniziale

---
+++ opera.php
@@ -0,0 +1,28 @@
+<?php
+/*Classe Opera: Rappresenta un'opera nel sito.
+* Serve a chi dovrà trasformare il risultato della ricerca (fornito come vettore di oggetti opera)
+ *in una pagina html. Oppure nel caso del dettaglio di un'opera soltanto viene ritornato un solo oggetto.
+*/
+
+class opera
+{
+    var $id;
+    var $titolo;
+    var $descrizione;
+    var $tipo;
+
+
+    /**
+     * Costruttore dell'oggetto opera.
+     * Tipicamente si presuppone che i dati gli arrivino dal risultato di una query "SELECT", già nel formato corretto,
+     * quindi non ci sono controlli/property/getter/setter ecc.
+     */
+    function __construct($id, $titolo, $descrizione, $tipo)
+    {
+        $this->tipo=$tipo;
+        $this->titolo=$titolo;
+        $this->descrizione=$descrizione;
+        $this->tipo=$tipo;
+    }
+}
+
