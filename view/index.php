bced94d1df4d95515c3d6477eaabf8bd0c33e42a
SugarFree <vittoriocorrizzato@gmail.com>
2018-02-05 15:07:16 +0100

    Bottoni cambiati

--- view/index.php
+++ view/index.php
@@ -1,11 +1,7 @@
 		<h2>The journey starts here!</h2>
 		<p id='descrizione'>Questo sito &egrave; stato creato nell'ambito del progetto di Tecnologie Web dell'anno accademico 2017/2018. Il sito si propone di associare i set di film, anime e serie televisive famosi con i rispettivi luoghi &quot;della vita di ogni giorno&quot; fornendo allo stesso tempo informazioni su di essi.</p>
-		<a href='ricerca_per_localita.html' class='shiny-button'>
-		<strong>Ricerca per luogo</strong>
-		</a>
-		<a href='ricerca_per_titolo.html' class='shiny-button2'>
-		<strong>Ricerca per titolo</strong>
-		</a>
+		<a href='../ricerca_per_localita.php'><button type="button">Cerca localit&agrave;</button></a>
+		<a href='cerca_opere.php'><button type="button">Cerca opere</button></a>
 		<h3>Top Searched</h3>
 		<img src='./img/bar-cesaroni.jpg' alt='' />
 		<p id='didascalia'>Bar &quot;Garbatella&quot; dalla serie televisiva &quot;I Cesaroni&quot;</p>
