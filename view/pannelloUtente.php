50b9083f9a0ad524473274aff8b8a28d08633c06
Enrico Zen <zenenrico96@gmail.com>
2018-02-08 21:14:59 +0100

    Try Catch inserito

--- view/pannelloUtente.php
+++ view/pannelloUtente.php
@@ -1,10 +1,34 @@
 <h2>Pannello di controllo</h2>
 <?php 
-	require_once 'connessione.php';
-	$query= mysqli_query($conn, "SELECT email FROM utente WHERE username='".$_SESSION['username']."'");
-	$arr = mysqli_fetch_array($query);
-	$result= mysqli_query($conn, "SELECT privilegi FROM utente WHERE username='".$_SESSION['username']."'");
-	$arr2 = mysqli_fetch_array($result);
+	require_once '../connessione.php';
+	session_start();
+	try
+    {
+        if(!isset($_SESSION['username']))
+        {
+            throw new Exception("Devi fare il login per visualizzare i dati richiesti");
+        }
+        $query = mysqli_query($conn, "SELECT email FROM utente WHERE username='" . $_SESSION['username'] . "'");
+        if(mysqli_error($conn)!="")
+        {
+            throw new Exception("Errore del database:".mysqli_error($conn));
+        }
+        $arr = mysqli_fetch_array($query);
+        $result = mysqli_query($conn, "SELECT privilegi FROM utente WHERE username='" . $_SESSION['username'] . "'");
+        if(mysqli_error($conn)!="")
+        {
+            throw new Exception("Errore del database:".mysqli_error($conn));
+        }
+        $arr2 = mysqli_fetch_array($result);
+
+        //QUI VA L'ECHO DELLA FORM
+    }
+    catch (Exception $e)
+    {
+        echo $e->getMessage();
+
+    }
+
 ?>
 <dl>
 	<dt class='grassetto' > Username: </dt><dd><?php echo($_SESSION['username']) ?></dd>
