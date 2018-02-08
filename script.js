20d1de2b3c2a5ba95d1045525860f45a1e7b6516
SugarFree <vittoriocorrizzato@gmail.com>
2018-02-08 16:14:29 +0100

    Aggiornamento 08/02

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onscroll = function() {
 	scrollFunction() };
 
+window.onload = function() {
+	document.getElementById('Top').style.display = 'none'; };
+
 function scrollFunction() {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
 		document.getElementById('Top').style.display = 'block'; }
