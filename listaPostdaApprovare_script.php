85631294a03808e08a7f7d729ebd79e494cab766
Enrico Zen <zenenrico96@gmail.com>
2018-02-08 14:59:49 +0100

    Mette tutti i post ancora da approvare in un array

---
+++ listaPostdaApprovare_script.php
@@ -0,0 +1,28 @@
+<?php
+require_once "connessione.php";
+require_once "post.php";
+require_once "funzioni.php";
+//Salva tutti i post in attesa di approvazione nell'array $array_post
+$username=$_SESSION["username"];
+try
+{
+    if (!isset($_SESSION["username"]))
+    {
+        throw  new  Exception("Devi loggarti per poter effettuare questa azione");
+    }
+    if(!amministratore($username))
+    {
+        throw  new  Exception("Devi essere un amministratore per poter effettuare questa azione");
+    }
+    $risultato = mysqli_query($conn,"SELECT * FROM post WHERE approvato=FALSE");
+
+    if (mysqli_error($conn)!="")
+    {
+        throw new Exception("Errore del database:". mysqli_error($conn));
+    }
+    $array_post = risultato_array_post($risultato);//Contiene i post in attesa di approvazione
+}
+catch (Exception $e)
+{
+    echo $e->getMessage();
+}
