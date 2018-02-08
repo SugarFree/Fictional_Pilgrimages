be5244045c035191a1b2ea14365308cdda923aaa
SugarFree <vittoriocorrizzato@gmail.com>
2018-02-08 15:58:56 +0100

    Aggiornamento 08/02

--- bottom.php
+++ bottom.php
@@ -1,7 +1,7 @@
 
 	</div>
 	<div id='scroll'>
-		<a href="#titolo"><img id='Top' src='./img/back_on_top.png' alt='' /></a>
+		<a href="#titolo"><img id='Top' src='./img/back_on_top.png' alt='Ritorna in cima' /></a>
 	</div>
 	<div id='footer'>
 		Copyright &copy; 2017 Fictional Pilgrimages
