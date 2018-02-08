fb66ed4a517dc56e574b9122f19877ca137e8e25
Zorua <1337.rainb0w@gmail.com>
2018-02-08 19:41:28 +0100

    fix php, fix errori xthml

--- top.php
+++ top.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'
-		'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
 <html xmlns='http://www.w3.org/1999/xhtml' lang='it' xml:lang='it'>
 <head>
 	<title><?php echo $titolo; ?></title>
 	<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-	<script src='script.js'></script>
-	<link href='https://fonts.googleapis.com/css?family=Crimson+Text:600' rel='stylesheet'>
+	<script type='text/javascript' src='script.js'></script>
+	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Crimson+Text:600' />
 	<link rel='stylesheet' type='text/css' href='style.css' />
 	<link rel='stylesheet' type='text/css' media='handheld, screen and (max-width:480px), only screen and (max-device-width:480px)' href='small_style.css' />
 	<link rel="stylesheet" type="text/css" media="print" href="print_style.css"/>
@@ -16,20 +15,19 @@
 		Ti trovi in: <?php echo $path . "\n";?>
 		<?php
 		session_start();
-		if(!isset($_SESSION['username']))
-		{
-
-			echo ("<form method='get' action='registrazione.php'>
-				<button type='submit'>Connettiti</button>
-			</form>");
-		}
-		else
-		{
-			echo ("<form method='get' action='logout.php'>
-			<button type='submit'>Logout</button>
-			</form>");
-			echo "<p id='prova'>Ciao " .$_SESSION['username']. "</p>";
-		}
+		if(!isset($_SESSION['username'])) {
+			echo ("<form method='get' action='registrazione.php'>\n" .
+				"\t\t\t<div>\n" .
+				"\t\t\t\t<button type='submit'>Connettiti</button>\n" .
+				"\t\t\t</div>\n" .
+				"\t\t</form>\n"); }
+		else {
+			echo ("<form method='get' action='logout.php'>\n" .
+				"\t\t\t<div>\n" .
+				"\t\t\t\t<button type='submit'>Logout</button>\n" .
+				"\t\t\t</div>\n" .
+				"\t\t</form>\n" .
+				"\t\t<p id='prova'>Ciao " . $_SESSION['username'] . "</p>\n");	}
 		?>
 	</div>
 	<div id='sidenav'>
@@ -41,26 +39,21 @@
 					$this->href=$href;
 					$this->icon=$icon;
 					$this->testo=$text; }}
+
+			$menu_items = array(new menu_item("index.php","home.png","Home"),
+				new menu_item("ricerca_per_localita.php","world.png","Cerca localit&agrave;"),
+				new menu_item("cerca_opere.php","titolo.png","Cerca opere"));
+
 			if(isset($_SESSION['username']))
-			{
-				$menu_items = array(new menu_item("index.php","home.png","Home"),
-														new menu_item("ricerca_per_localita.php","world.png","Cerca localit&agrave;"),
-														new menu_item("cerca_opere.php","titolo.png","Cerca opere"),
-														new menu_item("pannelloUtente.php","utente.png","Pannello utente"));
-			}
-			else
-			{
-				$menu_items = array(new menu_item("index.php","home.png","Home"),
-														new menu_item("ricerca_per_localita.php","world.png","Cerca localit&agrave;"),
-														new menu_item("cerca_opere.php","titolo.png","Cerca opere"));
-			}
+				$menu_items[] = new menu_item("pannelloUtente.php","utente.png","Pannello utente");
+
 			echo "\n";
 			for($i=0; $i<count($menu_items); $i++) {
 				echo "\t\t\t";
 				if($i == $current_menu_item) {
 					echo "<li class='selected'><img class='icon' src='./img/" . $menu_items[$i]->icon . "' alt='' />" . $menu_items[$i]->testo . "</li>"; }
 				else {
-					echo "<a href='" . $menu_items[$i]->href . "'><li><img class='icon' src='./img/" . $menu_items[$i]->icon . "' alt='' />" . $menu_items[$i]->testo . "</li></a>"; }
+					echo "<li><a href='" . $menu_items[$i]->href . "'><img class='icon' src='./img/" . $menu_items[$i]->icon . "' alt='' />" . $menu_items[$i]->testo . "</a></li>"; }
 				echo "\n"; }
 			?>
 		</ul>
