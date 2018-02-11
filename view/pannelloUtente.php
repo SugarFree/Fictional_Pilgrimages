964ed56c72b41a8378cc758900b7a9a39cf43a7f
Zorua <1337.rainb0w@gmail.com>
2018-02-11 12:37:57 +0100

    fix html

--- view/pannelloUtente.php
+++ view/pannelloUtente.php
@@ -11,24 +11,29 @@
 	catch (Exception $e) {
 		echo $e->getMessage(); }
 ?>
+
 		<h2>Pannello di controllo</h2>
 		<dl>
-			<dt>Username:</dt>
+			<dt lang='en'>Username:</dt>
 			<dd><?php echo($_SESSION["username"]) ?></dd>
 			<dt>Email:</dt>
 			<dd><?php echo $arr["email"]; ?></dd>
-			<dt>Tipo di utente:</dt>
-			<dd><?php echo $arr2["privilegi"]; ?></dd>
+			<dt>Rango:</dt>
+			<dd><?php
+					if($arr2["privilegi"] == "user")
+						echo "Utente";
+					else if($arr2["privilegi"] == "admin")
+						echo "Amministratore"; ?></dd>
 		</dl>
 		<div id='cambio'>
 			<h2>Cambio password</h2>
 			<form method='post' action='./cambio_password_script.php'>
 				<fieldset>
-					<label for='vecchia_password'>Vecchia password: </label>
+					<label for='vecchia_password'>Vecchia password:</label>
 					<input type='password' id='vecchia_password' name='password_vecchia' />
-					<label for='password'>Nuova password: </label>
+					<label for='password'>Nuova password:</label>
 					<input type='password' id='password' name='password' />
-					<label for='conferma_password'>Conferma nuova password: </label>
+					<label for='conferma_password'>Conferma nuova password:</label>
 					<input type='password' id='conferma_password' name='conferma_password' />
 					<input type='submit' value='Cambia password' />
 				</fieldset>
