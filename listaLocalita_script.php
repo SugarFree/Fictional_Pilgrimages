d0c55f383be6b516349b20b65ec9467278f2a3cb
Zorua <1337.rainb0w@gmail.com>
2018-02-11 17:20:58 +0100

    cerca_localita.php completato

--- listaLocalita_script.php
+++ listaLocalita_script.php
@@ -16,11 +16,16 @@ try
         throw new Exception($errore);
     }
 
+    $risultato=array();
     while (true) {
         $risultato_query=mysqli_fetch_array($query, MYSQLI_NUM);
         if($risultato_query==NULL) break;
-        $risultato[$i][0]= $risultato_query[0];
-        $risultato[$i][1] = $risultato_query[1];
+        if(isset($risultato[$risultato_query[0]]))
+        	array_push($risultato[$risultato_query[0]], $risultato_query[1]);
+        else
+			$risultato[$risultato_query[0]]=array($risultato_query[1]);
+        //$risultato[$i][0]= $risultato_query[0];
+        //$risultato[$i][1] = $risultato_query[1];
 
         $i++; }
 }
--- view/cerca_localita.php
+++ view/cerca_localita.php
@@ -1,8 +1,16 @@
 
 		<h2>Lista localit&agrave;:</h2>
-		<ul><?php
-				include __DIR__ . "../../listaTitoli_script.php";
-				for($i=0; $i<count($risultato); $i++) {
-					echo "\t\t\t<li><a href='./opera.php?nome=$risultato[$i]'>" . $risultato[$i] . "</a></li>\n"; }
-			?>
+		<ul>
+<?php
+	include __DIR__ . "../../listaLocalita_script.php";
+	//var_dump($risultato);
+	foreach($risultato as $stato => $localita) {
+		echo "\t\t\t<li>\n" .
+			"\t\t\t\t<h3>$stato</h3>\n" .
+			"\t\t\t\t<ul>\n";
+		for($i=0; $i<count($localita); $i++)
+			echo "\t\t\t\t\t<li><a href='./localita.php?nome=$localita[$i]'>$localita[$i]</a></li>\n";
+		echo "\t\t\t\t</ul>\n" .
+			"\t\t\t</li>\n"; }
+?>
 		</ul>
