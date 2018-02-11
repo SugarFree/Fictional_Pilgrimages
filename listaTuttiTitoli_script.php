7d7b6335b9c34b31231179cee96600d7ec5cd98d
Enrico Zen <zenenrico96@gmail.com>
2018-02-11 23:30:05 +0100

    Tutti i titoli anche quelli non usati

---
+++ listaTuttiTitoli_script.php
@@ -0,0 +1,28 @@
+<?php
+//Ritorna, una sola volta, tutti i titoli delle opere inserite, anche di quelle che non sono utilizzate in nessun post
+require_once "connessione.php";
+$i = 0;
+try
+{
+    $query = mysqli_query($conn, "SELECT titolo FROM opera ORDER BY titolo ASC ");
+    if ($query == FALSE)
+    {
+        throw new Exception("Errore del database:" . mysqli_error($conn));
+    }
+
+    while (true)
+    {
+        $risultato_query = mysqli_fetch_array($query, MYSQLI_NUM);
+        if ($risultato_query == NULL)
+            break;
+        $risultato[$i] = $risultato_query[0];
+
+        $i++;
+    }
+}
+catch (Exception $e)
+{
+    $risultato = array();
+}
+var_dump($risultato);
+//La lista dei titoli si trova in $risultato
