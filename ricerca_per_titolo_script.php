aec7dc4b921cb669c0b3b3e1911191e41fba670a
Zorua <1337.rainb0w@gmail.com>
2018-02-11 14:42:39 +0100

    opera.php completato

--- ricerca_per_titolo_script.php
+++ ricerca_per_titolo_script.php
@@ -1,42 +1,28 @@
 <?php
-
 require_once "connessione.php";
 require_once "funzioni.php";
 
-$titolo=$_POST["titolo"];
-
+$titolo=$_GET["nome"];
 $errore="";
-
 $titolo=trim(strip_tags($titolo));
 
-try
-{
+try {
+	if (empty($titolo)) { //Se non è stato specificato un titolo, ritorna tutti i post
+		// Non occorre usare un prepared statement perchè non ci sono input di nessun tipo
+		$risultato = mysqli_query($conn, "SELECT * FROM post WHERE approvato=TRUE");
+		if ($risultato == FALSE) {
+			$errore = mysqli_error($conn);
+			throw new Exception($errore); }}
+	else {
+		$risultato = $conn->prepare("SELECT * FROM post WHERE approvato=TRUE AND titolo_opera=?");
+		$risultato->bind_param("s", $titolo);
+		$risultato->execute();
+		if ($risultato->error != "") {
+			$errore = mysqli_error($conn);
+			throw new Exception($errore); }
+		$risultato = $risultato->get_result(); }
 
-    if (empty($titolo))//Se non è stato specificato un titolo, ritorna tutti i post
-    {
-        //Non occorre usare un prepared statement perchè non ci sono input di nessun tipo
-
-        $risultato = mysqli_query($conn, "SELECT * FROM post WHERE approvato=TRUE ORDER BY localita ASC ");
-        if ($risultato == FALSE) {
-            $errore = mysqli_error($conn);
-            throw new Exception("Errore del database: " . $errore);
-        }
-    } else {
-        $risultato = $conn->prepare("SELECT * FROM post WHERE approvato=TRUE AND titolo_opera=? ORDER BY localita ASC");
-        $risultato->bind_param("s", $titolo);
-        $risultato->execute();
-        if ($risultato->error != "") {
-            $errore = mysqli_error($conn);
-            $errore = ("Errore del database: " . $errore);
-            throw new Exception($errore);
-        }
-        $risultato = $risultato->get_result();
-    }
-
-//L'array di oggetti "post" si trova in $risultato. Se non c'è nessun risultato ritorna FALSE.
-    $risultato = risultato_array_post($risultato);
-}
-catch (Exception $e)
-{
-    echo "ERRORE: ".  $e->getMessage();
-}
+	// L'array di oggetti "post" si trova in $risultato. Se non c'è nessun risultato ritorna FALSE.
+	$risultato = risultato_array_post($risultato); }
+catch (Exception $e) {
+	echo "ERRORE: ".  $e->getMessage(); }
--- view/opera.php
+++ view/opera.php
@@ -1,7 +1,6 @@
 
+		<h2><?php echo $opera->titolo; ?></h2>
 		<dl>
-			<dt>Titolo:</dt>
-			<dd><?php echo $opera->titolo; ?></dd>
 			<dt>Tipo:</dt>
 			<dd><?php if($opera->tipo == "serietv")
 							echo "Serie TV";
@@ -9,7 +8,24 @@
 							echo "Film"; ?></dd>
 			<dt>Sinossi:</dt>
 			<dd><?php echo $opera->descrizione; ?></dd>
-<?php
-
-?>
 		</dl>
+<?php
+	include __DIR__ . "../../ricerca_per_titolo_script.php";
+
+	// Preparo array ordinato per località
+	$post = array();
+	foreach($risultato as $val)
+		$post[$val->localita][] = (array)$val;
+
+	// Lo scorro e per ciascuna località mostro la lista dei post
+	echo "\t\t<ul>\n";
+	foreach($post as $val) {
+		echo "\t\t\t<li>\n";
+		echo "\t\t\t\t<h3><a href='./localita?nome=" . $val[0]["localita"] . "'>" . $val[0]["localita"] . "</a></h3>\n";
+		echo "\t\t\t\t<ul>\n";
+		for($i=0; $i<count($val); $i++)
+			echo "\t\t\t\t\t<li><a href='./post.php?id=" . $val[$i]["id"] . "'><img src='./uploads/" . $val[$i]["id"] . ".jpg' alt='" . $val[$i]["descrizione"] ."' /></a></li>\n";
+		echo "\t\t\t\t</ul>\n";
+		echo "\t\t\t</li>\n"; }
+	echo "\t\t</ul>\n";
+?>
