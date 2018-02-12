98bb460730f572e3b30d827401cfa696d7279a79
Zorua <Zorua@users.noreply.github.com>
2018-02-12 11:16:07 +0100

    Update post.php

--- post.php
+++ post.php
@@ -2,7 +2,7 @@
 	include "dettaglioPost_script.php";
 
 	$titolo="ERRORE - Fictional Pilgrimages";
-	if(isset($risultato_post) && $risultato_post !== false)
+	if(isset($risultato_post) && !is_null($risultato_post))
 		$titolo="Post " . $risultato_post->titolo_opera . " - Fictional Pilgrimages";
 		$path="<a href='opera.php?nome=" . $risultato_post->titolo_opera . "'>" . $risultato_post->titolo_opera . "</a> &gt; " .
 			"<a href='localita.php?nome=". $risultato_post->localita . "'>" . $risultato_post->localita . "</a>";
