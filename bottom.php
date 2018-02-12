1b6d5c77deb711b83b4df27457b9230c48fec30b
Zorua <1337.rainb0w@gmail.com>
2018-02-12 05:30:18 +0100

    fix footer

--- bottom.php
+++ bottom.php
@@ -4,7 +4,7 @@
 		<a href='#titolo'><img id='Top' src='./img/back_on_top.png' alt='Ritorna in cima' /></a>
 	</div>
 	<div id='footer'>
-		<span id='copyright'>&copy; 2018 <span lang='en'>Fictional Pilgrimages</span></span>
+		&copy; 2018 <span lang='en'>Fictional Pilgrimages</span>
 	</div>
 </body>
 </html>
