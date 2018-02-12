bf3366d289efd283939b6260b49f030f4756b8cc
Faietto <andreadido96gmail.com>
2018-02-12 02:23:09 +0100

    12/02

--- view/aggiungi_opera.php
+++ view/aggiungi_opera.php
@@ -2,8 +2,8 @@
 		<h2>Aggiungi opera</h2>
 		<form method='post' enctype='multipart/form-data' action='inserimento_opera_script.php '>
 			<fieldset>
-				<label for='titolo_opera'>Seleziona l'opera raffigurata dalla tua fotografia tra quelle nella tendina:</label>
-				<input type='text' id='titolo_opera' name='titolo' />
+				<label for='nuova_opera'>Inserisci il titolo della nuova opera:</label>
+				<input type='text' id='nuova_opera' name='titolo' />
 				<label for='descrizione_opera'>Inserisci una breve descrizione:</label>
 				<input type='text' id='descrizione_opera' name='descrizione' />
 				<label for='tipo_opera'>Seleziona la tipologia dell'opera:</label>
--- view/connettiti.php
+++ view/connettiti.php
@@ -1,37 +1,33 @@
 
 		<div id='registrazione'>
-			<h2>Registrati</h2>
-			<form method='post' action='registrazione.php'>
-				<fieldset>
-					<label for='mail'>Inserisci la tua <span lang='en'>email</span>:</label>
-					<input type='text' id='mail' name='email' />
-					<label for='uname_reg'>Inserisci l'<span lang='en'>username</span> che desideri:</label>
-					<input type='text' id='uname_reg' name='username' />
-					<label for='pw_reg'>Inserisci una <span lang='en'>password</span>:</label>
-					<input type='password' id='pw_reg' name='password' />
-					<label for='conferma_pw'>Inseriscila di nuovo per conferma:</label>
-					<input type='password' id='conferma_pw' name='conferma_password' />
+			<h2>Registrazione</h2>
+			<form name='form_registration' method='post' action='registrazione.php'>
+				<label for='mail'>Email:</label>
+				<input type='text' id='mail' name='email' placeholder='La tua mail'>
+				<label for='Uname'>Username:</label>
+				<input type='text' id='Uname' name='username' placeholder='Il tuo username'>
+				<label for='pw'>Password:</label>
+				<input type='password' id='pw' name='password' placeholder='La tua password'>
+				<label for='conferma_pw'>Conferma password:</label>
+				<input type='password' id='conferma_pw' name='conferma_password' placeholder='Riscrivi la tua password'>
 <?php
 	if(isset($_GET['destination']))
-		echo "\t\t\t\t\t<input type='hidden' name='destination' value='" . htmlspecialchars($_GET['destination'], ENT_QUOTES) . "' />\n";
+		echo "\t\t\t\t<input type='hidden' name='destination' value='" . htmlspecialchars($_GET['destination'], ENT_QUOTES) . "' />\n";
 ?>
-					<input type='submit' value='Registrati' />
-				</fieldset>
+				<input type='submit' value='Registrati'>
 			</form>
 		</div>
 		<div id='login'>
-			<h2>Accedi</h2>
-			<form method='post' action='login.php'>
-				<fieldset>
-					<label for='uname_log'>Inserisci il tuo <span lang='en'>username</span>:</label>
-					<input type='text' id='uname_log' name='username' />
-					<label for='pw_log'>Inserisci la tua <span lang='en'>password</span>:</label>
-					<input type='password' id='pw_log' name='password' />
+			<h2>Login</h2>
+			<form name='form_registration' method='post' action='login.php'>
+				<label for='Uname'>Username:</label>
+				<input type='text' id='Uname' name='username' placeholder='Il tuo username'>
+				<label for='pw'>Password:</label>
+				<input type='password' id='pw' name='password' placeholder='La tua password'>
 <?php
 	if(isset($_GET['destination']))
-		echo "\t\t\t\t\t<input type='hidden' name='destination' value='" . htmlspecialchars($_GET['destination'], ENT_QUOTES) . "' />\n";
+		echo "\t\t\t<input type='hidden' name='destination' value='" . htmlspecialchars($_GET['destination'], ENT_QUOTES) . "' />\n";
 ?>
-					<input type='submit' value='Accedi' />
-				</fieldset>
+				<input type='submit' value='Login'>
 			</form>
 		</div>
