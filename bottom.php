2300456a1c580e06d21226f341aef8162e4bc6b8
Zorua <Zorua@users.noreply.github.com>
2018-02-12 10:39:37 +0100

    Update bottom.php

--- bottom.php
+++ bottom.php
@@ -1,6 +1,6 @@
 
 	</div>
-	<a id='to_top' href='#titolo'></a>
+	<a id='to_top' href='#titolo'>Ritorna in cima.</a>
 	<div id='footer'>
 		&copy; 2018 <span lang='en'>Fictional Pilgrimages</span>
 	</div>
