5d2dd63dcd6324b4c72b84a3196f6f604a3e06dd
Zorua <Zorua@users.noreply.github.com>
2018-02-12 10:45:15 +0100

    Update path.php

--- view/path.php
+++ view/path.php
@@ -3,22 +3,22 @@
 <?php
 	session_start();
 	if(!isset($_SESSION["username"])) {
-		echo ("\t\t<span id='saluto'>\n" .
+		echo ("\t\t<div id='saluto'>\n" .
 			"\t\t\t<form method='get' action='connettiti.php'>\n" .
 			"\t\t\t\t<fieldset>\n" .
 			"\t\t\t\t\t<input type='hidden' name='destination' value='" . $_SERVER["REQUEST_URI"] . "' />\n" .
 			"\t\t\t\t\t<button type='submit'>Connettiti</button>\n" .
 			"\t\t\t\t</fieldset>\n" .
 			"\t\t\t</form>\n" .
-			"\t\t</span>"); }
+			"\t\t</div>"); }
 	else {
-		echo ("\t\t<span id='saluto'>Ciao " . $_SESSION['username'] . "!\n" .
+		echo ("\t\t<div id='saluto'>Ciao " . $_SESSION['username'] . "!\n" .
 			"\t\t\t<form method='post' action='logout.php'>\n" .
 			"\t\t\t\t<fieldset>\n" .
 			"\t\t\t\t\t<input type='hidden' name='destination' value='" . $_SERVER["REQUEST_URI"] . "' />\n" .
 			"\t\t\t\t\t<button type='submit'>Sconnettiti</button>\n" .
 			"\t\t\t\t</fieldset>\n" .
 			"\t\t\t</form>\n" .
-			"\t\t</span>\n");	}
+			"\t\t</div>\n");	}
 ?>
 	</div>
